6bf97e1fcd6969215d8dd03325e09f3fa87932eb
komeno <tkomeno@student.42tokyo.jp>
2023-10-17 16:10:36 +0900

    created souces/, includes/ with the basic structure.

---
+++ includes/philo.h
@@ -0,0 +1,23 @@
+#ifndef PHILO_H
+#define PHILO_H
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                 libraries                                  */
+/*                                                                            */
+/* ************************************************************************** */
+
+//essential libraries
+#include <pthread.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+//other libraries
+#include <limits.h>
+#include <stdbool.h>
+
+//temporary libraries
+#include <stdio.h>
+
+#endif

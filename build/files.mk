773fd33979ef9caafe70b2ee5769f37a7b1ce94e
komeno <tkomeno@student.42tokyo.jp>
2023-11-17 10:26:35 +0900

    Added new files to Makefile.

--- build/files.mk
+++ build/files.mk
@@ -1,4 +1,10 @@
-FILES		=	main.c
+FILES		=	main.c \
+					input_validation/incorrect_input.c \
+					input_validation/validators/incorrect_num_of_args.c \
+					input_validation/validators/not_only_digits.c \
+					input_validation/validators/wrong_num_philos.c \
+					input_validation/validators/helpers/is_digit.c \
+					input_validation/validators/helpers/my_atoi.c
 
 SRC_DIR = sources
 OBJ_DIR		=	objects
